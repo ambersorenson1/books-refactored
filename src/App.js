ea0ead23b3aa8d0cd40fc6a0a2e6f37993cea538
Amber <asorenson@klipsu.com>
2023-02-10 10:50:23 -0600

    updated the app to store books in a local json file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import BookCreate from './components/BookCreate';
 import BookList from "./components/BookList";
 
+
 function App() {
     const [books, setBooks] = useState([]);
+
+const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+};
+
+useEffect(() => {
+    fetchBooks();
+}, []);
+
 
 const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
@@ -23,10 +35,14 @@ const deleteBookById = (id) => {
 setBooks(updatedBooks);
 };
 
-const createBook = (title) => {
+const createBook = async (title) => {
+   const response = await axios.post('http://localhost:3001/books', {
+        title: title
+    });
 const updatedBooks = [
-    ...books, { id: Math.floor(Math.random() * 9999), title: title }
+    ...books, response.data
 ];
+
 setBooks(updatedBooks);
 };
 
