096cacbd1ec92f76f6a83f9adee057a62f7d4a6d
Amber <asorenson@klipsu.com>
2023-02-10 13:46:30 -0600

    updated the app to use axios for editing, deleting, and persisiting

--- src/App.js
+++ src/App.js
@@ -17,17 +17,21 @@ useEffect(() => {
 }, []);
 
 
-const editBookById = (id, newTitle) => {
+const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books${id}`, {
+        title: newTitle
+    });
     const updatedBooks = books.map((book) => {
         if(book.id === id ){
-            return {...books, title: newTitle};
+            return {...books, ...response.data};
         }
         return book;
     });
     setBooks(updatedBooks);
 };
 
-const deleteBookById = (id) => {
+const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books${id}`)
        const updatedBooks = books.filter((book) => {
         return book.id !== id;
        }) 
