d30e3f14d7b472af5363aea4e5c415645b45274d
Amber <asorenson@klipsu.com>
2023-02-17 14:28:27 -0600

    wrapped the app in the Provider context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,15 @@
-import { useContext } from "react";
-import BooksContext from "../context/books";
-import BookShow from "./BookShow";
+import { useContext } from 'react';
+import BooksContext from '../context/books';
+import BookShow from './BookShow';
 
-function BookList () {
-const {books} = useContext(BooksContext)
-const renderedBooks = books.map((book) => {
+function BookList() {
+  const { books } = useContext(BooksContext);
+
+  const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} />;
-})
-    return<div className="book-list">{renderedBooks}</div>;
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
